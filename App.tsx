5fa68377eb8cabb38af1742547a30e1fb8186761
brendoncronos <brendongomes39@gmail.com>
2025-02-22 00:05:41 +0000

    meubotao

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.title}>Olá Elias!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ff0055',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+    margin: 10,
+    borderRadius: 30,
+    elevation:10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 5, 
+      height: 10,
+    },
+    shadowOpacity: 0.5
   },
+  title: {
+    fontSize: 40,
+    color: 'yellow',
+    fontWeight:"500",
+  } 
 });
